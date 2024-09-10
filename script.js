f99f54ebc624b5f8061e5c2912de103d57a6ea0d
Rylan Dalpez <rylandalpez@Rylans-MacBook-Air-2.local>
2024-09-10 03:49:44 -0700

    Simplify checkbox code. Fix rounding probability.

--- script.js
+++ script.js
@@ -1,33 +1,21 @@
 const container = document.querySelector("#container");
 const note = document.querySelector("#note");
-const lowEString = document.querySelector("#low-e-string");
-const aString = document.querySelector("#a-string");
-const dString = document.querySelector("#d-string");
-const gString = document.querySelector("#g-string");
-const bString = document.querySelector("#b-string");
-const highEString = document.querySelector("#high-e-string");
+const strings = Array.from(document.querySelectorAll(".string-name"));
 
 const notes = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
 
 function getStringText() {
     let checkedBoxes = [];
-    if (lowEString.checked)
-        checkedBoxes.push("E string");
-    if (aString.checked)
-        checkedBoxes.push("A string");
-    if (dString.checked)
-        checkedBoxes.push("D string");
-    if (gString.checked)
-        checkedBoxes.push("G string");
-    if (bString.checked)
-        checkedBoxes.push("B string");
-    if (highEString.checked)
-        checkedBoxes.push("e string");
+    for (const str of strings) {
+        if (str.checked) {
+            checkedBoxes.push(str.value + " string");
+        }
+    }
     
     if (checkedBoxes.length == 0)
         return "";
     else {
-        const index = Math.round(Math.random() * (checkedBoxes.length - 1));
+        const index = Math.floor(Math.random() * (checkedBoxes.length));
         return " on the " + checkedBoxes[index];
     }
 }
